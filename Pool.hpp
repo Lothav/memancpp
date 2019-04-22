f0c534495158eccdae15345a79f0dfcc39016c07
Luiz0tavio <me@luizotav.io>
2019-04-22 11:39:57 -0300

    Pool template to header

--- Pool.hpp
+++ Pool.hpp
@@ -25,11 +25,46 @@ namespace mem
 
     public:
 
-        explicit Pool(std::size_t size);
-        ~Pool();
+        explicit Pool(std::size_t size)
+        {
+            // allocate pool
+            mem_pool_ = new T[size];
+            if (mem_pool_== nullptr) {
+                std::cerr << "Error malloc memory pool" << std::endl;
+            }
 
-        void* get();
-        void free(void *);
+            for (int i = 0; i < size; ++i) {
+                mem_map_[mem_pool_+i] = FREE;
+            }
+        }
+
+        ~Pool()
+        {
+            delete[] mem_pool_;
+        }
+
+        void* get()
+        {
+            if (mem_pool_ == nullptr) {
+                std::cerr << "Memory pool not allocated!" << std::endl;
+                return nullptr;
+            }
+
+            for (auto& it = mem_map_.begin(); it != mem_map_.end(); it++) {
+                if(it->second == FREE) {
+                    it->second = IN_USE;
+                    return it->first;
+                }
+            }
+
+            std::cerr << "No free memory found! Try to increase pool size!" << std::endl;
+            return nullptr;
+        }
+
+        void free(void * ptr)
+        {
+            mem_map_[ptr] = FREE;
+        };
     };
 }
 
