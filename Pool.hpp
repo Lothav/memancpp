7cb78d040f948577437dc968228b0e5bb1353e4f
Luiz0tavio <me@luizotav.io>
2019-04-23 12:56:48 -0300

    dynamic realloc pool if necessary

--- Pool.hpp
+++ Pool.hpp
@@ -21,21 +21,15 @@ namespace mem
         T* mem_pool_ = nullptr;
         std::map<T*, MemStatus> mem_map_ = {};
 
-        std::size_t size_;
+        std::size_t initial_size_;
+        uint32_t mem_increases_count_ = 0;
 
     public:
 
-        explicit Pool(std::size_t size) : size_(size)
+        explicit Pool(std::size_t initial_size) : initial_size_(initial_size)
         {
-            // allocate pool
-            mem_pool_ = (T *) std::malloc(size * sizeof(T));
-            if (mem_pool_ == nullptr) {
-                std::cerr << "Error malloc memory pool" << std::endl;
-            }
-
-            for (int i = 0; i < size; ++i) {
-                mem_map_[&mem_pool_[i]] = FREE;
-            }
+            // allocate initial pool
+            increasePool();
         }
 
         ~Pool()
@@ -43,7 +37,25 @@ namespace mem
             std::free(mem_pool_);
         }
 
-        void* get()
+        void increasePool()
+        {
+            mem_increases_count_++;
+            mem_pool_ = (T *) std::realloc(mem_pool_, getPoolSize());
+            if (mem_pool_ == nullptr) {
+                std::cerr << "Error malloc memory pool" << std::endl;
+            }
+
+            for (size_t i = initial_size_ * (mem_increases_count_-1); i < initial_size_ * mem_increases_count_; ++i) {
+                mem_map_[&mem_pool_[i]] = FREE;
+            }
+        }
+
+        std::size_t getPoolSize()
+        {
+            return initial_size_ * mem_increases_count_ * sizeof(T);
+        }
+
+        void* getMemory()
         {
             if (mem_pool_ == nullptr) {
                 std::cerr << "Memory pool not allocated!" << std::endl;
@@ -57,8 +69,12 @@ namespace mem
                 }
             }
 
-            std::cerr << "No free memory found! Try to increase pool size!" << std::endl;
-            return nullptr;
+            std::cerr << "No free memory found in Pool of type '" << typeid(T).name() << "' with size " << std::to_string(getPoolSize()) << "!" << std::endl;
+            std::cerr << "Trying to increase pool size... ";
+            increasePool();
+            std::cerr << "Success! New size: " << std::to_string(getPoolSize()) << "." << std::endl;
+
+            return getMemory();
         }
 
         void free(void * ptr)
